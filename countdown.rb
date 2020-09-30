ddc5e3b8b59630fead832be5ca8a907b2d0daa5a
ocawthorne <ocawthorne@gmail.com>
2020-09-30 22:38:04 +0100

    countdowntomidnight-093020

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(time)
+    until time == 0
+        puts "#{time} SECOND(S)!\n"
+        time -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(time)
+    until time == 0
+        puts "#{time} SECOND(S)!\n"
+        time -= 1
+        sleep 1
+    end
+    "HAPPY NEW YEAR!"
 end
